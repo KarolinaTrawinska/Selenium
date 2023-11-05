410af688590ecc224b971f7b892b275418eec89e
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-11-05 13:13:53 +0100

    HomePageTest

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,9 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HomePageTest {
     private WebDriver driver;
@@ -28,13 +32,34 @@ public class HomePageTest {
     }
 
     //    Test
+//    @Test
+//    void shouldReturnCorrectPageTitle() {
+////        Metoda, która uruchamia przeglądarkę i otwiera podaną w argumencie stronę internetową
+//        driver.get("http://www.automationpractice.pl/index.php");
+//
+////        Sprawdzenie tytułu strony
+//        Assertions.assertThat(driver.getTitle()).isEqualTo("My Shop");
+//    }
     @Test
-    void shouldReturnCorrectPageTitle() {
-//        Metoda, która uruchamia przeglądarkę i otwiera podaną w argumencie stronę internetową
+    public void seeBestsellersOnHomePage() {
         driver.get("http://www.automationpractice.pl/index.php");
 
-//        Sprawdzenie tytułu strony
-        Assertions.assertThat(driver.getTitle()).isEqualTo("My Shop");
+        List<WebElement> link = driver.findElements(By.linkText("BEST SELLERS"));
+        link.get(0).click();
 
+        List<WebElement> productNamesByCssSelectors = driver.findElements(By.cssSelector("#blockbestsellers .product-name"));
+
+        List<WebElement> productNamesByxPath = driver.findElements(By.xpath("//*[@id='blockbestsellers']//*[@class='product-name']"));
+
+        for (WebElement productName : productNamesByCssSelectors) {
+            System.out.println(productName.getText());
+        }
+
+        System.out.println();
+        System.out.println();
+
+        for (WebElement productName : productNamesByxPath) {
+            System.out.println(productName.getText());
+        }
     }
 }

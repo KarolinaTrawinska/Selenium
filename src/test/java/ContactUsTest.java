c307ce47788ed6c3214fe9daa917ef9a5390137a
Karolina Trawińska <karolina.trawinska.95@gmail.com>
2023-11-05 16:10:42 +0100

    WaitingForElements

--- src/test/java/ContactUsTest.java
+++ src/test/java/ContactUsTest.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import net.bytebuddy.pool.TypePool;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,7 +9,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class ContactUsTest {
@@ -33,6 +38,13 @@ public class ContactUsTest {
 
     @Test
     public void shouldNotAllowToSendEmptyContactForm() {
+
+//      Czekanie na elementy1
+//            ImplicitlyWait - dodaje się do drivera, globalnie dla wszystkich elementów (findElement). Za każdym razem
+//            będzie czekać maksymalnie 10 sekund. Jeśli element będzie gotowy wcześniej to kod pójdzie dalej.
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         driver.get("http://www.automationpractice.pl/index.php");
         Assertions.assertThat(driver.getTitle()).isEqualTo("My Shop");
 
@@ -42,8 +54,17 @@ public class ContactUsTest {
         WebElement sendButton = driver.findElement(By.id("submitMessage"));
         sendButton.click();
 
-//        Asercja sprawdza czy element alertBox jest wyświetlany. Sprawdzamy czy to jest prawda
+//        Czekanie na elementy2
+//          ExplicitWait - mówimy na jaki konkretnie element chcemy poczekać np. taki który się wolniej ładuje na stronie.
+//          tworzymy nowy obiekt WebDriverWait. Podaje się warunek na ktory chce się czekać - widoczność elementu alertBox
+
         WebElement alertBox = driver.findElement(By.className("alert-danger"));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(alertBox));
+
+
+//        Asercja sprawdza czy element alertBox jest wyświetlany. Sprawdzamy czy to jest prawda
         Assertions.assertThat(alertBox.isDisplayed()).isTrue();
 
     }
